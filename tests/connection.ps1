2776a4b6ea24103853aea2b300a66a4faab8862e
alipouw13 <alipouw@gmail.com>
2025-07-14 14:26:12 -0400

    update test connection

--- tests/connection.ps1
+++ tests/connection.ps1
@@ -1,4 +1,4 @@
-$pair = "31b203dd44d14a17830e87d8017a072e:59857d53d34249e1b7586b8aa71be169"
+$pair = "un:pw"
 $encoded = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($pair))
 curl -X POST -H "Authorization: Basic $encoded" -d "grant_type=client_credentials" https://accounts.spotify.com/api/token
 
